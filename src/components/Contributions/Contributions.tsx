f80f4f258c751cecbdb26143ab84511717564ba9
Mel Andrew Getutua <melandrewgetutua@gmail.com>
2024-06-19 21:12:07 +0800

    Edited images path

--- src/components/Contributions/Contributions.tsx
+++ src/components/Contributions/Contributions.tsx
@@ -7,7 +7,7 @@ type ContributionsProps = {
 
 const projectData: ContributionsProps[] = [
   {
-    image: '/projects/screencapture-harriswilliams-2024-06-10-15_39_56.png',
+    image: 'projects/screencapture-harriswilliams-2024-06-10-15_39_56.png',
     title: 'Harris Williams',
     link: 'https://www.harriswilliams.com/',
     techStack: [
@@ -22,7 +22,7 @@ const projectData: ContributionsProps[] = [
     ],
   },
   {
-    image: '/projects/screencapture-nrgmr-2024-06-10-15_48_06.png',
+    image: 'projects/screencapture-nrgmr-2024-06-10-15_48_06.png',
     title: 'National Research Group (NRG)',
     link: 'https://www.nrgmr.com/',
     techStack: [
@@ -36,7 +36,7 @@ const projectData: ContributionsProps[] = [
     ],
   },
   {
-    image: '/projects/screencapture-zetaglobal-2024-06-10-15_50_03.png',
+    image: 'projects/screencapture-zetaglobal-2024-06-10-15_50_03.png',
     title: 'Zeta Global',
     link: 'https://zetaglobal.com/',
     techStack: [
@@ -49,7 +49,7 @@ const projectData: ContributionsProps[] = [
     ],
   },
   {
-    image: '/projects/screencapture-golubcapital-2024-06-10-15_52_02.png',
+    image: 'projects/screencapture-golubcapital-2024-06-10-15_52_02.png',
     title: 'Golub Capital',
     link: 'https://golubcapital.com/',
     techStack: [
@@ -63,13 +63,13 @@ const projectData: ContributionsProps[] = [
   },
   {
     image:
-      '/projects/screencapture-commonwealthfund-org-2024-06-10-16_05_15.png',
+      'projects/screencapture-commonwealthfund-org-2024-06-10-16_05_15.png',
     title: 'Commonwealth Fund',
     link: 'https://www.commonwealthfund.org/',
     techStack: ['Drupal', 'HTML', 'SCSS'],
   },
   {
-    image: '/projects/screencapture-velveta-jp-2024-06-10-16_16_12.png',
+    image: 'projects/screencapture-velveta-jp-2024-06-10-16_16_12.png',
     title: 'Velveta',
     link: 'https://www.velveta.jp/',
     techStack: [
@@ -82,7 +82,7 @@ const projectData: ContributionsProps[] = [
   },
   {
     image:
-      '/projects/screencapture-attack-inc-co-jp-media-lp-2024-06-14-18_19_36.png',
+      'projects/screencapture-attack-inc-co-jp-media-lp-2024-06-14-18_19_36.png',
     title: 'Attack Inc Media',
     link: 'https://www.attack-inc.co.jp/media-lp/',
     techStack: [
@@ -94,7 +94,7 @@ const projectData: ContributionsProps[] = [
     ],
   },
   {
-    image: '/projects/screencapture-south22-clinic-2024-06-14-18_20_11.png',
+    image: 'projects/screencapture-south22-clinic-2024-06-14-18_20_11.png',
     title: 'South Clinic',
     link: 'https://south22-clinic.com/',
     techStack: [
@@ -106,7 +106,7 @@ const projectData: ContributionsProps[] = [
     ],
   },
   {
-    image: '/projects/screencapture-isikuni-co-jp-2024-06-14-18_20_59.png',
+    image: 'projects/screencapture-isikuni-co-jp-2024-06-14-18_20_59.png',
     title: 'Ishikuni',
     link: 'https://isikuni.co.jp/',
     techStack: [
@@ -118,7 +118,7 @@ const projectData: ContributionsProps[] = [
     ],
   },
   {
-    image: '/projects/screencapture-fullmark-omiya-2024-06-14-19_23_34.png',
+    image: 'projects/screencapture-fullmark-omiya-2024-06-14-19_23_34.png',
     title: 'Fullmark Omiya',
     link: 'https://fullmark-omiya.com/',
     techStack: [
@@ -144,6 +144,7 @@ const Contributions = () => {
                 {/* project image */}
                 <div className='basis-[30%] max-w-[150px] max-h-[90px] flex-shrink-0 overflow-hidden'>
                   <img
+                    loading='lazy'
                     src={image}
                     alt={title}
                     className='w-full h-full object-cover'
